1bd7b44ce20cc05cb5957e80c2a1121fd41210f7
Ashish Kumar <60581051+zzdish@users.noreply.github.com>
2021-06-11 23:29:07 +0530

    Delete authy directory

--- authy/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from authy.models import Profile
-# Register your models here.
-
-admin.site.register(Profile)
--- authy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthyConfig(AppConfig):
-    name = 'authy'
